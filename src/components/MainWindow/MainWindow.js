08f44ccc51ae1b5d7248b2232d84a0c5ae260448
rashed <x.suwaidi@gmail.com>
2021-09-18 13:53:25 +0000

    WIP filter

--- src/components/MainWindow/MainWindow.js
+++ src/components/MainWindow/MainWindow.js
@@ -5,9 +5,15 @@ const MainWindow = () => {
 
     return (
         <div className="main">
-            <SearchBar className=''/>
-            <h1 className='main__title'>Hello Rashed</h1>
-            <h3 className='main__subtitle'>Welcome Back</h3>
+            <SearchBar className='' />
+            <div>
+                <h1 className='main__title'>Hello Rashed</h1>
+                <h4 className='main__subtitle'>Welcome Back!</h4>
+            </div>
+            <div className='main__filter'>
+                <p>Filters</p>
+                
+            </div>
         </div>
     )
 
