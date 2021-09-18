cdff82a8409b69caf863260ede387be2490e82b5
rashed <x.suwaidi@gmail.com>
2021-09-18 12:58:10 +0000

    add title

--- src/components/MainWindow/MainWindow.js
+++ src/components/MainWindow/MainWindow.js
@@ -6,6 +6,8 @@ const MainWindow = () => {
     return (
         <div className="main">
             <SearchBar className=''/>
+            <h1 className='main__title'>Hello Rashed</h1>
+            <h3 className='main__subtitle'>Welcome Back</h3>
         </div>
     )
 
